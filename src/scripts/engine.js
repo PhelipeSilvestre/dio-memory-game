dc8f9668740484754183df32d67a25b1d3aa939f
Phelipe_Silvestre <phelipe17silvestre@gmail.com>
2024-10-26 16:30:17 -0300

    update

---
+++ src/scripts/engine.js
@@ -0,0 +1,24 @@
+const emojis = [
+  "😎",
+  "😎",
+  "🤑",
+  "🤑",
+  "🤡",
+  "🤡",
+  "👽",
+  "👽",
+  "💀",
+  "💀",
+  "🐱‍👤",
+  "🐱‍👤",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box);
+}
